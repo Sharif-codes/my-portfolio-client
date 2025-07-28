185b8f35ad6dc282a8540987f77862372a5f9fb1
sharif-codes <sharifxenjia@gmail.com>
2025-07-29 00:54:01 +0600

    edit in about section

--- app/components/AboutTabs.jsx
+++ app/components/AboutTabs.jsx
@@ -3,16 +3,17 @@ const AboutTabs = ({setSelectedTabs,selectedTabs}) => {
      
    
     return (
-         <div className="flex md:gap-16 gap-7">
+         <div className="flex md:gap-12 gap-7">
             <div className="text-sm lg:text-md">
               <button onClick={()=>setSelectedTabs("education")} className={`${selectedTabs === "education" ? "text-orange-500 border-b" : ""} cursor-pointer`}>Education</button>
             </div>
             <div className="text-sm lg:text-md">
-              <button onClick={()=>setSelectedTabs("course")} className={`${selectedTabs === "course" ? "text-orange-500 border-b" : ""} cursor-pointer`}>Courses</button>
+              <button onClick={()=>setSelectedTabs("hobby")} className={`${selectedTabs === "hobby" ? "text-orange-500 border-b" : ""} cursor-pointer`}>Interpersonal</button>
             </div>
             <div className="text-sm lg:text-md">
-              <button onClick={()=>setSelectedTabs("hobby")} className={`${selectedTabs === "hobby" ? "text-orange-500 border-b" : ""} cursor-pointer`}>Hobbies</button>
+              <button onClick={()=>setSelectedTabs("course")} className={`${selectedTabs === "course" ? "text-orange-500 border-b" : ""} cursor-pointer`}>Courses</button>
             </div>
+            
             <div className="text-sm lg:text-md" >
               <button onClick={()=>setSelectedTabs("language")} className={`${selectedTabs === "language" ? "text-orange-500 border-b" : ""} cursor-pointer`}>Languages</button>
             </div>
--- app/components/Hobby.jsx
+++ app/components/Hobby.jsx
@@ -1,13 +1,17 @@
-import { MdOutlineSportsCricket, MdOutlineTour } from "react-icons/md";
+import { MdGroups2 } from "react-icons/md";
 import { GiCycling } from "react-icons/gi";
-
+import { IoBook, IoExtensionPuzzle, IoHeart, IoPulseSharp, IoTime} from "react-icons/io5";
 const Hobby = () => {
     return (
         <div className="mt-2 flex flex-col justify-center items-center text-sm lg:text-md">
             <ul className="space-y-1">
-                <li className="flex items-center gap-1 text-orange-400"> <span><MdOutlineTour /> </span>  Travelling</li>
-                <li className="flex items-center gap-1 text-orange-400"> <span> <MdOutlineSportsCricket /> </span>  Playing Cricket</li>
-                <li className="flex items-center gap-1 text-orange-400"> <span> <GiCycling /> </span> Cycling</li>
+                <li className="flex items-center gap-2 text-orange-400"> <span><MdGroups2 /> </span> A team player who loves to assist his team.</li>
+                <li className="flex items-center gap-2 text-orange-400"> <span> <IoExtensionPuzzle /> </span>Interest in problem solving.</li>
+                <li className="flex items-center gap-2 text-orange-400"> <span> <IoTime /></span> Managing time effectively.</li>
+
+                <li className="flex items-center gap-2 text-orange-400"> <span> <IoHeart /></span>Having a positive mindset in every situation.</li>
+                <li className="flex items-center gap-2 text-orange-400"> <span> <IoPulseSharp /></span>Maintaining sincerity and  proffessionalism in job.</li>
+                <li className="flex items-center gap-2 text-orange-400"> <span> <IoBook /></span>Always learning mindset.</li>
                 
             </ul>
         </div>
