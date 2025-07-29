45e2dd7e0682c582aa50080e1e948e5c8a71d782
sharif-codes <sharifxenjia@gmail.com>
2025-07-29 12:27:14 +0600

    responsiveness

--- app/pages/ProjectsPage/Projects.jsx
+++ app/pages/ProjectsPage/Projects.jsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 const Projects = () => {
 
     return (
-        <div className="mx-4 lg:mx-50 py-5 lg:py-0">
+        <div className="mx-4 lg:mx-50 pt-5 pb-12 lg:py-0">
             <h2 className="text-xl lg:text-3xl text-center py-3 lg:py-8">My <span className="text-orange-500">Projects</span></h2>
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-5 mt-4">
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-7 mt-4">
                 {
                     projects?.map(project => (
 
@@ -21,7 +21,7 @@ const Projects = () => {
                                 className="w-full h-full rounded-lg object-cover group-hover:opacity-15 transition-opacity duration-500"
                             >
                             </Image>
-                            <div className="absolute inset-0  transition-normal  flex gap-4 flex-col items-center justify-center opacity-0 group-hover:opacity-90 transition-opacity duration-200">
+                            <div className="absolute inset-0  transition-normal hidden  md:flex gap-4 flex-col items-center justify-center opacity-0 group-hover:opacity-90 transition-opacity duration-200">
 
                                 <Link href={project.live_link} target="_blank">
                                     <button className="rounded-sm cursor-pointer w-16 bg-orange-400">Live</button>
@@ -34,6 +34,18 @@ const Projects = () => {
                                         <button className="rounded-sm cursor-pointer w-16 bg-purple-400">Server</button>
                                     </Link>
                                 </div>
+                            </div>
+                            <div className="flex justify-center gap-1 md:hidden">
+                                <Link href={project.live_link} target="_blank">
+                                    <button className="rounded-sm cursor-pointer w-11 h-4 text-xs bg-orange-400">Live</button>
+                                </Link>
+                                <Link href={project.client} target="_blank">
+                                    <button className="rounded-sm cursor-pointer w-11 h-4 text-xs bg-sky-400">Client</button>
+                                </Link>
+                                <Link href={project.server} target="_blank">
+                                    <button className="rounded-sm cursor-pointer w-11 h-4 text-xs bg-purple-400">Server</button>
+                                </Link>
+
                             </div>
                         </div>
                     ))
